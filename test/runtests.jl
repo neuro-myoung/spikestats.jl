4b22dde22806de9a1083fa90930a8848c151224e
MIchael Young <michael.young@duke.edu>
2020-12-15 01:49:04 -0500

    fixed cv test

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,6 +7,6 @@ println(names(SpikeStats))
 
 @testset "SpikeStats.jl" begin
     @test typeof(testset) == Array{Float64,1}
-    @test round(computeCV(diff(testset)), digits=2) == 1.07
+    @test round(computeCV(diff(testset)), digits=2) == 0.93
     @test round(computeFano(testset,0.001), digits=2) == 0.90
 end
